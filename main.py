e5bc6fc7c20a2cb8dfb3b6c86cb0375f22db27cd
Adhoc <adhoc@fennecdev.com>
2021-10-13 16:36:40 -0700

    deleting logging - back to testing

--- main.py
+++ main.py
@@ -5,11 +5,7 @@ from discord.ext import commands
 from discord.utils import get
 
 # Logging
-logging.basicConfig(filename='cerberus_diloup.log',
-                    encoding='utf-8',
-                    level=logging.DEBUG,
-                    format='%(asctime)s %(levelname)-8s %(message)s',
-                    datefmt='%Y-%m-%d %H:%M:%S')
+logging.basicConfig()
 logging.info("Starting Logging...")
 
 # Config bot
